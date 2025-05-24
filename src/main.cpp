036bb8a368193b592986ed9664db031606a1d721
Elijah Kibler <elijahkibler@mac.mynetworksettings.com>
2025-05-23 21:58:19 -0400

    work setting up cmake file

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+#include <SDL3/SDL_init.h>
+#include <SDL3_ttf/SDL_ttf.h>
+#include <SDL3_mixer/SDL_mixer.h>
+#include <SDL3_image/SDL_image.h>
+
+//Elijah Kibler
+//Right now we are just setting up the build enviroment but in the future this will contain the core game loop
+
+int main(){
+    std::cout<< "Hello World, this is to test if the sdl build worked" << std::endl;
+    return 0;
+}
